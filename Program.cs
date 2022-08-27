98609b7cdcce40cb9903383dfb7a72e62c0b3fd2
vonka2011 <108064137+vonka2011@users.noreply.github.com>
2022-08-27 23:48:09 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
-﻿Console.WriteLine("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int current =1;
+﻿// Написать программу, кот.принимает на вход цифру, кот.явл.днем недели и проверяет выходной ли это.
+void PrintDay(int num)
+{
+    if(num >0 && num < 8)
+    {
+        if (num == 6 || num==7) Console.WriteLine("Ура,выходной!");
+        else Console.WriteLine("Дуй на работу!");
 
-while(current <= N)
-{ 
-  if (current %2==0)
-  Console.Write(current +" ");
-current++;
+    }
+    else Console.WriteLine("такого дня нема");
+
 }
-
-
-
-
+Console.WriteLine("Виудите число дня недели");
+int numberOfDay = Convert.ToInt32(Console.ReadLine());
+PrintDay (numberOfDay);
 
